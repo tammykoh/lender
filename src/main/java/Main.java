6b58c347be388d8ff12f5e2abcb101bfd61e11a1
tammykoh <tammykohrw@gmail.com>
2020-08-31 20:18:37 +0800

    Changed to OOP

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,27 @@
 public class Main {
-    static int[] amounts = new int[100];
-    static String[] borrowers = new String[100];
-    static boolean[] isPaid = new boolean[100];
-    static double[] interestRates = new double[100];
+    static Loan[] Loans = new Loan[100];
     static int loansCount = 0;
 
     public static void addLoan(String borrower, int amount, double interestRate){
-        borrowers[loansCount] = borrower;
-        amounts[loansCount] = amount;
-        interestRates[loansCount] = interestRate;
+        Loan loan = new Loan(borrower, amount, interestRate);
+        Loans[loansCount] = loan;
         loansCount++;
     }
 
     public static void setAsPaid(String borrower){
         for (int i=0; i< loansCount; i++){
-            if (borrowers[i].equals(borrower)){
-                isPaid[i] = true;
+            Loan loan = Loans[i];
+            if (loan.getBorrower().equals(borrower)){
+                loan.setAsPaid();
             }
         }
     }
 
     public static void listLoans(){
         for (int i=0; i< loansCount; i++){
-            if (!isPaid[i]){
-                System.out.println(borrowers[i] + ": " + amounts[i]);
+            Loan loan = Loans[i];
+            if (!loan.getIsPaid()){
+                System.out.println(loan.getBorrower() + ": " + loan.getAmounts());
             }
         }
     }
@@ -31,9 +29,10 @@ public class Main {
     public static double calculateProfitEarned(){
         double profit = 0;
         for (int i=0; i< loansCount; i++){
+            Loan loan = Loans[i];
             // if the loan was repaid, add the interest to profit
-            if (isPaid[i]){
-                profit = profit + (amounts[i] * interestRates[i]);
+            if (loan.getIsPaid()){
+                profit = profit + (loan.getAmounts() * loan.getInterestRates());
             }
         }
         return profit;
